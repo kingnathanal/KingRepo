c4676f7af3a57e03a241b5465f056ea0f0514b2d
kingnathanal <kingnathanal@gmail.com>
2017-07-08 02:12:21 -0400

    Added another Animal to Program

--- ProjectFarm/src/com/projectfarm/core/Program.java
+++ ProjectFarm/src/com/projectfarm/core/Program.java
@@ -10,6 +10,10 @@ public class Program {
 		
 		System.out.println(animal.toString());
 		
+		Animals animal2 = new Animals("Will", "Red");
+		
+		System.out.println(animal2.toString());
+		
 		System.out.println("End of Farm Program");
 	}
 
