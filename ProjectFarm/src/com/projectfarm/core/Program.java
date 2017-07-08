1429d09e3fbc077dd22ce42b78e367e64c304a15
kingnathanal2 <kingnathanal2@gmail.com>
2017-07-08 10:31:29 -0400

    Add comments dummy

--- ProjectFarm/src/com/projectfarm/core/Program.java
+++ ProjectFarm/src/com/projectfarm/core/Program.java
@@ -6,10 +6,12 @@ public class Program {
 		
 		System.out.println("Welcome to the Animal Farm");
 		
+		// Create first animal here
 		Animals animal = new Animals("Ryan");
 		
 		System.out.println(animal.toString());
 		
+		// Create second animal here
 		Animals animal2 = new Animals("Will", "Red");
 		
 		System.out.println(animal2.toString());
