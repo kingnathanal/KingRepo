6bf6a251fae43f7e18ea8f02976da928dbee4d4c
kingnathanal <kingnathanal@gmail.com>
2017-07-08 02:09:59 -0400

    Added Color to Animals

--- ProjectFarm/src/com/projectfarm/core/Animals.java
+++ ProjectFarm/src/com/projectfarm/core/Animals.java
@@ -3,9 +3,16 @@ package com.projectfarm.core;
 public class Animals {
 	
 	private String name; 
+	private String color;
 	
 	public Animals(String name) {
 		setName(name); 
+		setColor("Black");
+	}
+	
+	public Animals(String name, String color) {
+		setName(name);
+		setColor(color);
 	}
 	
 	public String getName() {
@@ -16,9 +23,17 @@ public class Animals {
 		this.name = name;
 	}
 	
+	public String getColor() {
+		return color; 
+	}
+	
+	public void setColor(String color) {
+		this.color = color; 
+	}
+	
 	@Override
 	public String toString() {
-		return "This animals name is: " + name; 
+		return "This animals name is: " + name + " and it has a color of: " + color; 
 	}
 
 }
