8f86d3d7046a2aeb5d073ad58cd94812f679b425
kingnathanal <kingnathanal@gmail.com>
2017-08-19 13:10:19 -0400

    Changing 10 to 12

--- Java8Test/src/com/javatest/client/Client.java
+++ Java8Test/src/com/javatest/client/Client.java
@@ -9,7 +9,7 @@ public class Client {
 		
 		List<Integer> list = new ArrayList<>();
 		
-		list.add(10);
+		list.add(12);
 		list.add(100);
 		list.add(50);
 		list.add(60);
@@ -26,7 +26,9 @@ public class Client {
 		System.out.println("------------------------------------------------------------------ ");
 		
 			list.stream().filter(x -> x % 10 == 0).forEach(System.out::println);
-		}	
+		}
+	
+	   // Sweet Sweet Sweet
 		
 	}
 
